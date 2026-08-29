6499990241d91503513ce92792241e58a23caaae
Ajay Kumar <ak9428647@gmail.com>
2026-08-29 18:17:10 +0530

    add base templates for all modules of landing_page  and render module-specific components in main file of module

--- src/landing_page/signup/SignupPage.jsx
+++ src/landing_page/signup/SignupPage.jsx
@@ -1,21 +1,21 @@
 import Hero from "./Hero"
 import FAQS from "./FAQS"
+import DematAccount from "./DematAccount"
+import AccountTypes from "./AccountTypes"
 import Navbar from "../Navbar"
 import Footer from "../Footer"
-
-
-
+import OpenAccount from "../OpenAccount"
 
 export default function SignupPage(){
     return (
         <>
             <Navbar />
             <Hero/>
+            <DematAccount />
             <AccountTypes />
             <FAQS />
             <OpenAccount />
             <Footer />
-
         </>
     )
 }
